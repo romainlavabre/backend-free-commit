35bb709c40c2cd0a1e8448934a00a9edc3d88ac7
romainlavabre <romainlavabre98@gmail.com>
2022-02-06 21:15:54 +0100

    ADD Json annotation

--- src/main/java/com/free/commit/entity/Developer.java
+++ src/main/java/com/free/commit/entity/Developer.java
@@ -1,6 +1,9 @@
 package com.free.commit.entity;
 
+import com.free.commit.api.json.annotation.Group;
+import com.free.commit.api.json.annotation.Json;
 import com.free.commit.api.security.User;
+import com.free.commit.configuration.json.GroupType;
 
 import javax.persistence.*;
 
@@ -10,15 +13,31 @@ import javax.persistence.*;
 @Entity
 public class Developer {
 
+    @Json( groups = {
+            @Group( name = GroupType.ADMIN ),
+            @Group( name = GroupType.DEVELOPER )
+    } )
     @Id
     @GeneratedValue( strategy = GenerationType.IDENTITY )
     private long id;
 
+    @Json( groups = {
+            @Group( name = GroupType.ADMIN ),
+            @Group( name = GroupType.DEVELOPER )
+    } )
     @Column( name = "github_username" )
     private String githubUsername;
 
+    @Json( groups = {
+            @Group( name = GroupType.ADMIN ),
+            @Group( name = GroupType.DEVELOPER )
+    } )
     private String email;
 
+    @Json( groups = {
+            @Group( name = GroupType.ADMIN ),
+            @Group( name = GroupType.DEVELOPER )
+    } )
     @OneToOne( cascade = {CascadeType.PERSIST} )
     @JoinColumn( name = "user_id", unique = true, nullable = false )
     private User user;
