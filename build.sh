84d1357fde06b13df9910df1c9ed26396e4e1e4b
romainlavabre <romainlavabre98@gmail.com>
2022-02-15 23:00:57 +0100

    ADD live env

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker build -t romainlavabre/free-commit-api:"$1" -t romainlavabre/free-commit-api:latest -f ./docker/live/Dockerfile .
+
+docker login
+
+docker push romainlavabre/free-commit-api:"$1"
+docker push romainlavabre/free-commit-api:latest
+
+git tag -a "$1" -m "$1" && git push --tags
