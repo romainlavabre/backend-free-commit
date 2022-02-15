2c2aeb91967a45ffe8c6ffe808077559527a7a5f
romainlavabre <romainlavabre98@gmail.com>
2022-02-15 13:49:39 +0100

    ADD Guest public

--- src/main/java/com/free/commit/configuration/security/SecurityConfig.java
+++ src/main/java/com/free/commit/configuration/security/SecurityConfig.java
@@ -31,7 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/auth",
             "/api/documentation/**",
             "/v2/api-docs",
-            "/v3/api-docs"
+            "/v3/api-docs",
+            "/guest/**"
     };
     protected            JwtTokenHandler    jwtTokenHandler;
     protected            UserDetailsService userDetailsService;
