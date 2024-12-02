6f4f8d04588d43ed38c92dce6f57df4e86b8279f
romainlavabre <romainlavabre98@gmail.com>
2024-12-03 00:37:06 +0100

    ADD Statistic

--- src/main/resources/sql/sum-duration.sql
+++ src/main/resources/sql/sum-duration.sql
@@ -4,4 +4,5 @@ CONCAT(YEAR(BH.at),"-" ,MONTH(BH.at)) AS "date",
 SUM(BH.duration) AS "duration"
 FROM build_history BH
 WHERE {{condition}}
-GROUP BY CONCAT(YEAR(BH.at),"-" ,MONTH(BH.at))
+GROUP BY CONCAT(YEAR(BH.at),"-" ,MONTH(BH.at)), YEAR(BH.at), MONTH(BH.at)
+ORDER BY YEAR(BH.at), MONTH(BH.at) ASC
