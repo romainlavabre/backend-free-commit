45a02cf883b0436dfb35084a4d49600fa2f9739c
romainlavabre <romainlavabre98@gmail.com>
2024-12-02 20:48:43 +0100

    FIX secret pagination

--- src/main/java/com/free/commit/configuration/pagination/SecretPagination.java
+++ src/main/java/com/free/commit/configuration/pagination/SecretPagination.java
@@ -11,7 +11,7 @@ import org.romainlavabre.pagination.annotation.Pagination;
 /**
  * @author Romain Lavabre <romainlavabre98@gmail.com>
  */
-@Pagination( mode = ModeType.FILE, filePath = "classpath:sql/secret.sql" )
+@Pagination( mode = ModeType.FILE, filePath = "classpath:sql/secret.sql", allowSorting = false )
 @Entity
 public class SecretPagination {
 
--- src/main/resources/sql/secret.sql
+++ src/main/resources/sql/secret.sql
@@ -3,8 +3,6 @@ S.id AS id,
 S.id AS secret_id,
 S.name AS secret_name,
 S.env AS secret_env,
-IF(COUNT(SP.secret_id) > 0, "PRIVATE", "PUBLIC") AS secret_scope
+IF((SELECT COUNT(SP.secret_id) FROM secret_project SP WHERE S.id = SP.secret_id ) > 0, "PRIVATE", "PUBLIC") AS secret_scope
 FROM secret S
-LEFT JOIN secret_project SP ON S.id = SP.secret_id
 WHERE {{condition}}
-GROUP BY S.id
