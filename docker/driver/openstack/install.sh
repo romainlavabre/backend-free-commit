c1d12f04d005d5d3ac1296a9d822c8e0489c9e6d
romainlavabre <romainlavabre98@gmail.com>
2025-05-14 21:28:12 +0200

    Improve openstack driver

--- docker/driver/openstack/install.sh
+++ docker/driver/openstack/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Add Docker's official GPG key:
-sudo apt-get update && sudo apt upgrade -y
-sudo apt-get install ca-certificates curl
+sudo apt update
+sudo apt install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
@@ -12,8 +12,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
+sudo apt update
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin git -y
 
 sudo usermod -aG docker $USER
