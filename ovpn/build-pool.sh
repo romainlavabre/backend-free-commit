9421886fa44c7c622b6bb09a7e902cd374c2509b
romainlavabre <romainlavabre98@gmail.com>
2025-06-18 12:37:26 +0200

    [4.1.0] ADD OpenVPN

--- ovpn/build-pool.sh
+++ ovpn/build-pool.sh
@@ -3,16 +3,18 @@
 # Store the absolute path of the directory where the script is located
 script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-unsanitized_client="$1"
-client=$(sed 's/[^0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-]/_/g' <<< "$unsanitized_client")
-while [[ -z "$client" || -e /etc/openvpn/server/easy-rsa/pki/issued/"$client".crt ]]; do
-    echo "$client: invalid name."
-    read -p "Name: " unsanitized_client
+for ((i = 0 ; i < $1 ; i++)); do
+	unsanitized_client="$i"
     client=$(sed 's/[^0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-]/_/g' <<< "$unsanitized_client")
+    while [[ -z "$client" || -e /etc/openvpn/server/easy-rsa/pki/issued/"$client".crt ]]; do
+        echo "$client: invalid name."
+        read -p "Name: " unsanitized_client
+        client=$(sed 's/[^0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-]/_/g' <<< "$unsanitized_client")
+    done
+    cd /etc/openvpn/server/easy-rsa/
+    ./easyrsa --batch --days=3650 build-client-full "$client" nopass
+    # Build the $client.ovpn file, stripping comments from easy-rsa in the process
+    grep -vh '^#' /etc/openvpn/server/client-common.txt /etc/openvpn/server/easy-rsa/pki/inline/private/"$client".inline > "$script_dir"/"$client".ovpn
+    echo
+    echo "$client added. Configuration available in:" "$script_dir"/"$client.ovpn"
 done
-cd /etc/openvpn/server/easy-rsa/
-./easyrsa --batch --days=3650 build-client-full "$client" nopass
-# Build the $client.ovpn file, stripping comments from easy-rsa in the process
-grep -vh '^#' /etc/openvpn/server/client-common.txt /etc/openvpn/server/easy-rsa/pki/inline/private/"$client".inline > "$script_dir"/"$client".ovpn
-echo
-echo "$client added. Configuration available in:" "$script_dir"/"$client.ovpn"
